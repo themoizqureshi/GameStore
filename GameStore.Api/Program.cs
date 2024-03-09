23c8d3776d9fdb3e15aad2c17c39bcea4be1f7e1
Moiz Qureshi <155383697+themoizqureshi@users.noreply.github.com>
2024-03-09 12:25:28 +0530

    using ILogger

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -7,11 +7,21 @@ builder.Services.AddRepositories(builder.Configuration);
 
 builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddGameStoreAuthorization();
+builder.Services.AddHttpLogging(o => { });
+
+// builder.Logging.AddJsonConsole(options =>
+// {
+//     options.JsonWriterOptions = new()
+//     {
+//         Indented = true
+//     };
+// });
 
 var app = builder.Build();
 
 await app.Services.InitializeDb();
 
+app.UseHttpLogging();
 app.MapGamesEndpoints();
 
 app.Run();
